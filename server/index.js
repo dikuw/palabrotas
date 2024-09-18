b0bb0f08bf8af5caf74bf72ce4eaae8a19b17866
Michael Vasilevsky <michael@dikuw.com>
2024-09-18 13:40:18 -0400

    add DB and routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-// import { connectDB } from './database/index.js';
-
-// import configureRoutes from './routes/index.js';
+import { connectDB } from './database/index.js';
+import configureRoutes from './routes/index.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("This is the WI Writer backend/API.");
+  res.send("This is the Palabrotas backend/API.");
 });
 
 //  enable CORS for all origins to allow development with local server
