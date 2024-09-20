34e4c941bdcb24a71cb3620d92b4229071702162
Michael Vasilevsky <michael@dikuw.com>
2024-09-20 10:11:13 -0400

    delete unused App.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
-import './App.css';
+import './styles/styles.css';
 
 import { useContentStore } from './store/content';
 
