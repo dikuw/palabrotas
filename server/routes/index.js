acd8a0a620aa63eedb6a062e8f1699d364658aa1
Michael Vasilevsky <michael@dikuw.com>
2024-09-20 07:15:54 -0400

    add user routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 import contentRouter from './contentRoutes.js';
+import userRouter from './userRoutes.js';
 
 export default (app) => {
   app.use("/api/content", contentRouter);
+  app.use("/api/user", userRouter);
 };
