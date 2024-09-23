d9678b5a9c9f5e5c717b3149c57f9722dbca77eb
Michael Vasilevsky <michael@dikuw.com>
2024-09-23 06:35:05 -0400

    update formatting

--- client/src/components/main/Card.jsx
+++ client/src/components/main/Card.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledGridFigure = styled.figure`
-  flex-basis: calc(33.333% - 4rem);
-  flex-grow: 1;
-  flex-shrink: 0;
-  margin: 0 2rem 2rem 2rem;
+  width: 100%;
+  margin: 0 0 2rem 0;
   padding: 2rem;
   border: 1px solid var(--primary);
   background: var(--almostWhite);
   box-shadow: 0 0 0 5px rgba(0,0,0,0.03);
   position: relative;
-  max-width: 400px;
 `;
 
 export default function Card(props) {
