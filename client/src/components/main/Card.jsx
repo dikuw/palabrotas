1f8b242cb6d0b98ee20f03485083a0b6ff03f646
Michael Vasilevsky <michael@dikuw.com>
2024-09-29 08:09:03 -0400

    add edit button

--- client/src/components/main/Card.jsx
+++ client/src/components/main/Card.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from "react-i18next";
 import styled from 'styled-components';
 
 const StyledGridFigure = styled.figure`
@@ -11,10 +13,42 @@ const StyledGridFigure = styled.figure`
   position: relative;
 `;
 
+const StyledEditButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 0.6rem;
+  text-transform: uppercase;
+  font-weight: 400;
+  font-style: normal;
+  background: var(--primary);
+  border-color: var(--primary);
+  border-radius: 2px;
+  border: 0;
+  color: var(--almostWhite);
+  display: inline-block;
+  height: 35px;
+  letter-spacing: 1px;
+  line-height: 35px;
+  margin: 0;
+  padding: 0 15px;
+  transition: background-color 300ms ease-out;
+  width: auto;
+  cursor: pointer;
+`;
+
 export default function Card(props) {
+  const navigate = useNavigate();
+  const { t } = useTranslation();
   const { item } = props;
+  
+  const handleClick = (item) => {
+    navigate(`/editContent/${item._id}`);
+  };
+
   return (
   <StyledGridFigure>
+    <StyledEditButton onClick={() => handleClick(item)}>{t('Edit')}</StyledEditButton>
     <figcaption>
       <p>{item.title}</p>
       <p>{item.description}</p>
