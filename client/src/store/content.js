a28e4f6b3af5b635ab315fb4eeeb5eefb745e66f
Michael Vasilevsky <michael@dikuw.com>
2024-09-29 08:09:18 -0400

    add updateContent

--- client/src/store/content.js
+++ client/src/store/content.js
@@ -15,6 +15,18 @@ export const useContentStore = create((set) => ({
     set((state) => ({ contents: [...state.contents, data.data] }));
     return data;
   },
+  updateContent: async (updatedContent) => {
+    const res = await fetch("/api/content/updateContent", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedContent),
+    })
+    const data = await res.json();
+    set((state) => ({ contents: state.contents.map(content => content._id === data._id ? data : content) }));
+    return data;
+  },
   getContents: async () => {
     const res = await fetch("/api/content/getContents", {
       method: "GET",
