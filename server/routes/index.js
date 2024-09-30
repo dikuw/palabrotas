91f327eae01cc5b2a3aab44523b1b1c8e1d45b8a
Michael Vasilevsky <michael@dikuw.com>
2024-09-30 15:32:32 -0400

    add flashcardRouter

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import authRouter from './authRoutes.js';
 import userRouter from './userRoutes.js';
-import contentRouter from './contentRoutes.js';
+import contentRouter from './contentRoutes.js'; 
+import flashcardRouter from './flashcardRoutes.js';
 
 export default (app) => {
   app.use("/api/auth", authRouter);
   app.use("/api/user", userRouter);
   app.use("/api/content", contentRouter);
+  app.use("/api/flashcard", flashcardRouter);
 };
