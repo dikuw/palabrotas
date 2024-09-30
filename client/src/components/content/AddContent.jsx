df32f010173e747f225c6d77c3f003eaed898b15
Michael Vasilevsky <michael@dikuw.com>
2024-09-30 14:41:09 -0400

    add fields

--- client/src/components/content/AddContent.jsx
+++ client/src/components/content/AddContent.jsx
@@ -6,6 +6,8 @@ import styled from 'styled-components';
 import { useAuthStore } from '../../store/auth';
 import { useContentStore } from '../../store/content';
 
+import { countries } from '../shared/countries';
+
 import VisibleActionButton from '../shared/VisibleActionButton';  
 
 const StyledWrapperDiv = styled.div`
@@ -39,6 +41,35 @@ const StyledInput = styled.input`
   color: ${props => props.$hasError ? 'red' : 'inherit'};
 `;
 
+const StyledSelect = styled.select`
+  margin: 0.25rem;
+  padding: 10px;
+  font-size: 1rem;
+  background-color: ${props => props.$hasError ? 'var(--warning)' : 'var(--almostWhite)'};
+  color: ${props => props.$hasError ? 'red' : 'inherit'};
+  border: 2px solid ;
+  border-radius: 0; 
+  appearance: none;
+  // Add a custom dropdown arrow
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cpath fill='%23000000' d='M0 0l4 4 4-4z'/%3E%3C/svg%3E");
+  background-repeat: no-repeat;
+  background-position: right 10px center;
+  padding-right: 30px;
+  &:invalid {
+    color: #757575; // This color should match your input placeholder color
+  }
+
+  option {
+    color: inherit;
+  }
+
+  option:first-of-type {
+    color: #757575; // This color should match your input placeholder color
+  }
+`;
+
+
+
 export default function AddContent() {
   const navigate = useNavigate();
   const { t } = useTranslation();
@@ -47,6 +78,11 @@ export default function AddContent() {
   const [formData, setFormData] = useState({
     title: "",
     description: "",
+    country: "",
+    hint1: "",
+    hint2: "",
+    hint3: "",
+    exampleSentence: "",
     owner: authStatus.user ? authStatus.user._id : "66f97a0ef1de0db4e4c254eb",
   });
 
@@ -62,6 +98,7 @@ export default function AddContent() {
     let newErrors = {};
     if (!formData.title) newErrors.title = t("Please enter a title.");
     if (!formData.description) newErrors.description = t("Please enter a description.");
+    if (!formData.country) newErrors.country = t("Please select a country.");
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
@@ -98,6 +135,51 @@ export default function AddContent() {
           value={errors.description ? "" : formData.description}
           onChange={handleChange}
           $hasError={!!errors.description}
+        />
+        <StyledSelect
+          name="country"
+          value={formData.country}
+          onChange={handleChange}
+          $hasError={!!errors.country}
+        >
+          <option value="" disabled hidden>{t("Select a country")}</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.code}>
+              {t(country.name)}
+            </option>
+          ))}
+        </StyledSelect>
+        <StyledInput
+          name="hint1"
+          type="text"
+          placeholder={errors.hint1 || t("First hint")}
+          value={errors.hint1 ? "" : formData.hint1}
+          onChange={handleChange}
+          $hasError={!!errors.hint1}
+        />
+        <StyledInput
+          name="hint2"
+          type="text"
+          placeholder={errors.hint2 || t("Second hint")}
+          value={errors.hint2 ? "" : formData.hint2}
+          onChange={handleChange}
+          $hasError={!!errors.hint2}
+        />
+        <StyledInput
+          name="hint3"
+          type="text"
+          placeholder={errors.hint3 || t("Third hint")}
+          value={errors.hint3 ? "" : formData.hint3}
+          onChange={handleChange}
+          $hasError={!!errors.hint3}
+        />
+        <StyledInput
+          name="exampleSentence"
+          type="text"
+          placeholder={errors.exampleSentence || t("Example sentence")}
+          value={errors.exampleSentence ? "" : formData.exampleSentence}
+          onChange={handleChange}
+          $hasError={!!errors.exampleSentence}
         />
         <VisibleActionButton type="submit" buttonLabel={t("Add Content")} />
       </form>
