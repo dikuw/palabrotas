6378966246df5e26638442669e079d5e6b55d571
Michael Vasilevsky <michael@dikuw.com>
2024-10-15 17:36:33 -0400

    remove props

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -37,7 +37,7 @@ const StyledLegal = styled.div`
   }
 `;
 
-export default function Footer(props) {
+export default function Footer() {
   const { t } = useTranslation();
   return (
     <StyledFooter >
