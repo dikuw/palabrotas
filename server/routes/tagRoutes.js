b82892f436c8e3be775a924e8092a24e5e237cd4
Michael Vasilevsky <michael@dikuw.com>
2024-11-01 10:07:42 -0400

    add addTagToContent

--- server/routes/tagRoutes.js
+++ server/routes/tagRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getTags, addTag } from '../controllers/tagController.js';
+import { getTags, addTag, addTagToContent } from '../controllers/tagController.js';
 
 const router = express.Router();
 
 router.get("/getTags", getTags);
 router.post("/addTag", addTag);
-
+router.post("/addTagToContent", addTagToContent);
 export default router;  
