aee68b38afd87638798e848973295200e494ac84
Michael Vasilevsky <michael@dikuw.com>
2024-11-16 08:59:45 -0500

    add NoResults

--- client/src/components/main/Grid.jsx
+++ client/src/components/main/Grid.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useContentStore } from '../../store/content';
 import ContentGrid from './ContentGrid';
+import NoResults from './NoResults';
 
 export default function Grid() {
-  const { contents, searchResults } = useContentStore();
+  const { contents, isSearching, searchResults } = useContentStore();
   
   const displayContents = searchResults.length > 0 ? searchResults : contents;
+
+  if (isSearching && searchResults.length === 0) {
+    return (
+      <NoResults />
+    );
+  }
 
   return <ContentGrid contents={displayContents} showEditIcon={false} />;
 }
