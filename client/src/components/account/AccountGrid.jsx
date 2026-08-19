98648b1550036564003731386aff6ca2e94cf23e
Michael Vasilevsky <michael@dikuw.com>
2026-08-19 02:02:20 -0400

    handle no content

--- client/src/components/account/AccountGrid.jsx
+++ client/src/components/account/AccountGrid.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Trans } from 'react-i18next';
 import ContentGrid from '../main/ContentGrid';
 
+const EmptyMessage = styled.p`
+  text-align: center;
+  margin: 24px auto;
+  color: #555;
+  line-height: 1.5;
+  font-size: 0.95rem;
+`;
+
+const StyledLink = styled(Link)`
+  color: var(--primary);
+  font-weight: bold;
+  text-decoration: underline;
+`;
+
 export default function AccountGrid({ contents }) {
+  if (!contents?.length) {
+    return (
+      <EmptyMessage>
+        <Trans
+          i18nKey="You haven't added any content yet. Go here to <addContentLink>Add Content</addContentLink>"
+          components={{
+            addContentLink: <StyledLink to="/addContent" />,
+          }}
+        />
+      </EmptyMessage>
+    );
+  }
+
   return <ContentGrid contents={contents} showEditIcon={true} />;
 }
